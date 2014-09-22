db8bfc2f3df2fe1728773700f0624c8db0dc47c7
Sharique <shazomaniac@yahoo.com>
2014-09-22 19:33:27 +0530

    new repository

---
+++ main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void printdetail(int x[]);
+void main()
+{
+    int i;
+    int x[15];
+
+    for(i=0;i<15;i++)
+    {
+        x[i]=i;
+    }
+    printdetail(x);
+}
+void printdetail(int x[])
+{
+    int i;
+    for(i=0;i<15;i++)
+    {
+        printf("value in the array is %d and the address is %p\n\n",x[i],&x[i]);
+    }
+}
+
